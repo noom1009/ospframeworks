edcc29201b0d2cc9efb53fa17c4ffdc76f9c67c9
thanit.net <noom1009@gmail.com>
2024-10-08 13:59:08 +0700

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/noom1009/ospframeworks
+
+go 1.23.1
---
+++ ospframeworks.go
@@ -0,0 +1,6 @@
+package ospframeworks
+
+// HelloWorld ฟังก์ชันที่จะส่งคืนข้อความ "Hello, World!"
+func HelloWorld() string {
+    return "Hello, World! ospframeworks"
+}
